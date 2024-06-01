afec38b3da6fe8122914fe8556238ec8418b53aa
iliaatmazhitov <atmazhitovilya@outlook.com>
2024-06-01 16:30:06 +0300

    Initial Commit

---
+++ HackerRank-DSA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HackerRank-DSA
+//
+//  Created by Илья Атмажитов on 01.06.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
